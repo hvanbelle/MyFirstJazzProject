86d03f76896b717e81324fae80edbb12539d36c9
hvanbelle <hvanbelle@gmail.com>
2013-12-09 13:20:12 +0000

    changes in jade

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 MyFirstJazzProject
 ==================
-My First Jazz Project
+My First Jazz Project. 
+
+Started with: 
+
+   http://clock.co.uk/tech-blogs/a-simple-website-in-nodejs-with-express-jade-and-stylus
+
--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 /*
  * Module dependencies
  */
-var express = require('express')
-  , stylus = require('stylus')
-  , nib = require('nib')
+/*global __dirname console module require*/
+var express = require('express');
+var stylus = require('stylus');
+var nib = require('nib');
 
-
-var app = express()
+var app = express();
 
 function compile(str, path) {
   return stylus(str)
@@ -14,25 +14,25 @@ function compile(str, path) {
     .use(nib());
 }
 
-app.set('views', __dirname + '/views')
-app.set('view engine', 'jade')
-app.use(express.logger('dev'))
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.logger('dev'));
 app.use(stylus.middleware(
-  { src: __dirname + '/public'
-  , compile: compile
+  { src: __dirname + '/public', compile: compile
   }
-))
-app.use(express.static(__dirname + '/public'))
+));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
   res.render('index',
   { title : 'Home' }
-  )
-})
+  );
+});
+
 app.get('/jokes/young_monk', function (req, res) {
   res.render('young_monk',
   { title : 'YoungMonk' }
-  )
-})
+  );
+});
 
-app.listen(3000)
+app.listen(3000);
